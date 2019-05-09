2e9c3e0a3f9e0887b16fae6b22fb676daa3ee7c6
MattMCollins <matthew.collins@dariel.co.za>
2019-05-10 00:17:37 +0200

    added profile pictures

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { IWorker } from '../models/worker.model';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { DataGeneratorService } from '../services/data-generator/data-generator.service';
+import { WorkerProfileService } from '../services/worker-profile/worker-profile.service';
 
 @Component({
   selector: 'app-home',
@@ -12,99 +13,27 @@ import { DataGeneratorService } from '../services/data-generator/data-generator.
 export class HomePage {
  
   workers: IWorker[];
-  electricians: IWorker[] = [{
-    id: 1, name: 'Liam', surname: 'Smith', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 4.7,category: ''
-  },
+  electricians: IWorker[];
+  plumbers: IWorker[];
+  tilers: IWorker[];
+  builders: IWorker[];
+  painters: IWorker[];
+  handymen: IWorker[];
+  imgs: ['../../assets/1.jpg', '../../assets/2.jpg', '../../assets/3.jpg', '../../assets/4.jpg', '../../assets/5.jpg',
+  '../../assets/6.jpg', '../../assets/7.jpg', '../../assets/8.jpg', '../../assets/9.jpg', '../../assets/10.jpg',
+  '../../assets/11.jpg', '../../assets/12.jpg', '../../assets/13.jpg', '../../assets/14.jpg', '../../assets/15.jpg',
+  '../../assets/16.jpg', '../../assets/17.jpg', '../../assets/18.jpg', '../../assets/19.jpg', '../../assets/20.jpg',
+  '../../assets/21.jpg', '../../assets/22.jpg', '../../assets/23.jpg', '../../assets/24.jpg', '../../assets/25.jpg',
+  '../../assets/26.jpg', '../../assets/27.jpg', '../../assets/28.jpg', '../../assets/29.jpg', '../../assets/30.jpg'];
+  
+  constructor(private dataGenerator: DataGeneratorService, private workerService: WorkerProfileService)
   {
-    id: 2, name: 'Noah', surname: 'Johnson', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 4.3, category: ''
-  },
-  {
-    id: 3, name: 'William', surname: 'Williams', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 3.8, category: ''
-  },
-  {
-    id: 4, name: 'James', surname: 'Jones', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 4.4, category: ''
-  },
-  {
-    id: 5, name: 'Logan', surname: 'Brown', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 4.85, category: ''
-  }];
-  plumbers: IWorker[] = [{
-    id: 6, name: 'Benjamin', surname: 'Davis', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 4.12,category: ''
-  },
-  {
-    id: 7, name: 'Mason', surname: 'Miller', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 4.3, category: ''
-  },
-  {
-    id: 8, name: 'Elijah', surname: 'Wilson', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 4.0, category: ''
-  },
-  {
-    id: 9, name: 'Oliver', surname: 'Moore', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 3.9, category: ''
-  },
-  {
-    id: 10, name: 'Jacob', surname: 'Taylor', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 4.72, category: ''
-  }];
-  tilers: IWorker[] = [{
-    id: 11, name: 'Lucas', surname: 'Anderson', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 4.44,category: ''
-  },
-  {
-    id: 12, name: 'Michael', surname: 'Thomas', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 4.1, category: ''
-  },
-  {
-    id: 13, name: 'Alexander', surname: 'Jackson', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 4.6, category: ''
-  },
-  {
-    id: 14, name: 'Ethan', surname: 'White', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 3.85, category: ''
-  },
-  {
-    id: 15, name: 'Daniel', surname: 'Harris', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 3.9, category: ''
-  }];
-  builders: IWorker[] = [{
-    id: 16, name: 'Matthew', surname: 'Martin', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.7,category: ''
-  },
-  {
-    id: 17, name: 'Aiden', surname: 'Thompson', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.31, category: ''
-  },
-  {
-    id: 18, name: 'Henry', surname: 'Garcia', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.32, category: ''
-  },
-  {
-    id: 19, name: 'Joseph', surname: 'Martinez', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.4, category: ''
-  },
-  {
-    id: 20, name: 'Jackson', surname: 'Robinson', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.1, category: ''
-  }];
-  painters: IWorker[] = [{
-    id: 21, name: 'Samuel', surname: 'Clark', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 4.5,category: ''
-  },
-  {
-    id: 22, name: 'Sebastian', surname: 'Rodriguez', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 4.1, category: ''
-  },
-  {
-    id: 23, name: 'David', surname: 'Lewis', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 4.95, category: ''
-  },
-  {
-    id: 24, name: 'Carter', surname: 'E4S', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 4.25, category: ''
-  },
-  {
-    id: 25, name: 'Wyatt', surname: 'Lee', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 3.85, category: ''
-  }];
-  handymen: IWorker[] = [{
-    id: 26, name: 'Jayden', surname: 'Walker', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.3,category: ''
-  },
-  {
-    id: 27, name: 'John', surname: 'Hall', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.3, category: ''
-  },
-  {
-    id: 28, name: 'Owen', surname: 'Allen', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.7, category: ''
-  },
-  {
-    id: 29, name: 'Dylan', surname: 'Young', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.45, category: ''
-  },
-  {
-    id: 30, name: 'Luke', surname: 'Hernandez', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.6, category: ''
-  }];
-
-  constructor(private dataGenerator: DataGeneratorService)
-  {
-    this.workers = this.dataGenerator.workers;
+    // this.workers = this.dataGenerator.workers;
+    this.electricians = this.workerService.electricians;
+    this.plumbers = this.workerService.plumbers;
+    this.tilers = this.workerService.tilers;
+    this.builders = this.workerService.builders;
+    this.painters = this.workerService.painters;
+    this.handymen = this.workerService.handymen;
   }
 }
--- src/app/services/worker-profile/worker-profile.service.ts
+++ src/app/services/worker-profile/worker-profile.service.ts
@@ -7,42 +7,113 @@ import { DataGeneratorService } from '../data-generator/data-generator.service';
   providedIn: 'root'
 })
 export class WorkerProfileService {
-  categories: string[] = ['Electrician', 'Plumber', 'Tiler', 'Builder', 'Painter', 'Handyman'];
-  ids: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
-  names: string[] = ['Jason', 'Matthew', 'Yannick', 'Laura', 'Uvir', 'Matt', 'Jim', 'Thabiso'];
-  surnames: string[] = ['Jason', 'Collins', 'Yannick', 'Laura', 'Uvir', 'Matt', 'Jim', 'Thabiso'];
-  emails: string[] = ['Jason@blank.com', 'Matthew@blank.com', 'Yannick@blank.com', 'Laura@blank.com',
-    'Uvir@blank.com', 'Matt@blank.com', 'Jim@blank.com', 'Thabiso@blank.com'];
-  phoneNumbers: string[] = ['0829383838', '0829383838', '0829383838', '0829383838', '0829383838', '0829383838', '0829383838', '0829383838'];
-  skills: string[][] = [['Electrician', 'Plumber'], ['Electrician', 'Plumber'], ['Electrician', 'Plumber'],
-  ['Electrician', 'Plumber'], ['Electrician', 'Plumber'], ['Electrician', 'Plumber'],
-  ['Electrician', 'Plumber'], ['Electrician', 'Plumber']]
-  ratings: number[] = [3.4, 4.3, 4.8, 4.9, 5.0, 3.5, 3.5, 4.75];
+  // categories: string[] = ['Electrician', 'Plumber', 'Tiler', 'Builder', 'Painter', 'Handyman'];
+  // ids: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+  // names: string[] = ['Jason', 'Matthew', 'Yannick', 'Laura', 'Uvir', 'Matt', 'Jim', 'Thabiso'];
+  // surnames: string[] = ['Jason', 'Collins', 'Yannick', 'Laura', 'Uvir', 'Matt', 'Jim', 'Thabiso'];
+  // emails: string[] = ['Jason@blank.com', 'Matthew@blank.com', 'Yannick@blank.com', 'Laura@blank.com',
+  //   'Uvir@blank.com', 'Matt@blank.com', 'Jim@blank.com', 'Thabiso@blank.com'];
+  // phoneNumbers: string[] = ['0829383838', '0829383838', '0829383838', '0829383838', '0829383838', '0829383838', '0829383838', '0829383838'];
+  // skills: string[][] = [['Electrician', 'Plumber'], ['Electrician', 'Plumber'], ['Electrician', 'Plumber'],
+  // ['Electrician', 'Plumber'], ['Electrician', 'Plumber'], ['Electrician', 'Plumber'],
+  // ['Electrician', 'Plumber'], ['Electrician', 'Plumber']]
+  // ratings: number[] = [3.4, 4.3, 4.8, 4.9, 5.0, 3.5, 3.5, 4.75];
+
+  electricians: IWorker[] = [{
+    id: 1, name: 'Liam', surname: 'Smith', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 4.7,category: ''
+  },
+  {
+    id: 2, name: 'Noah', surname: 'Johnson', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 4.3, category: ''
+  },
+  {
+    id: 3, name: 'William', surname: 'Williams', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 3.8, category: ''
+  },
+  {
+    id: 4, name: 'James', surname: 'Jones', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 4.4, category: ''
+  },
+  {
+    id: 5, name: 'Logan', surname: 'Brown', email: '', password: '', phoneNumber: '', skills: ['Electrician'], rating: 4.85, category: ''
+  }];
+  plumbers: IWorker[] = [{
+    id: 6, name: 'Benjamin', surname: 'Davis', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 4.12,category: ''
+  },
+  {
+    id: 7, name: 'Mason', surname: 'Miller', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 4.3, category: ''
+  },
+  {
+    id: 8, name: 'Elijah', surname: 'Wilson', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 4.0, category: ''
+  },
+  {
+    id: 9, name: 'Oliver', surname: 'Moore', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 3.9, category: ''
+  },
+  {
+    id: 10, name: 'Jacob', surname: 'Taylor', email: '', password: '', phoneNumber: '', skills: ['Plumber'], rating: 4.72, category: ''
+  }];
+  tilers: IWorker[] = [{
+    id: 11, name: 'Lucas', surname: 'Anderson', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 4.44,category: ''
+  },
+  {
+    id: 12, name: 'Michael', surname: 'Thomas', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 4.1, category: ''
+  },
+  {
+    id: 13, name: 'Alexander', surname: 'Jackson', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 4.6, category: ''
+  },
+  {
+    id: 14, name: 'Ethan', surname: 'White', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 3.85, category: ''
+  },
+  {
+    id: 15, name: 'Daniel', surname: 'Harris', email: '', password: '', phoneNumber: '', skills: ['Tiler'], rating: 3.9, category: ''
+  }];
+  builders: IWorker[] = [{
+    id: 16, name: 'Matthew', surname: 'Martin', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.7,category: ''
+  },
+  {
+    id: 17, name: 'Aiden', surname: 'Thompson', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.31, category: ''
+  },
+  {
+    id: 18, name: 'Henry', surname: 'Garcia', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.32, category: ''
+  },
+  {
+    id: 19, name: 'Joseph', surname: 'Martinez', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.4, category: ''
+  },
+  {
+    id: 20, name: 'Jackson', surname: 'Robinson', email: '', password: '', phoneNumber: '', skills: ['Builder'], rating: 4.1, category: ''
+  }];
+  painters: IWorker[] = [{
+    id: 21, name: 'Samuel', surname: 'Clark', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 4.5,category: ''
+  },
+  {
+    id: 22, name: 'Sebastian', surname: 'Rodriguez', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 4.1, category: ''
+  },
+  {
+    id: 23, name: 'David', surname: 'Lewis', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 4.95, category: ''
+  },
+  {
+    id: 24, name: 'Carter', surname: 'E4S', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 4.25, category: ''
+  },
+  {
+    id: 25, name: 'Wyatt', surname: 'Lee', email: '', password: '', phoneNumber: '', skills: ['Painter'], rating: 3.85, category: ''
+  }];
+  handymen: IWorker[] = [{
+    id: 26, name: 'Jayden', surname: 'Walker', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.3,category: ''
+  },
+  {
+    id: 27, name: 'John', surname: 'Hall', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.3, category: ''
+  },
+  {
+    id: 28, name: 'Owen', surname: 'Allen', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.7, category: ''
+  },
+  {
+    id: 29, name: 'Dylan', surname: 'Young', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.45, category: ''
+  },
+  {
+    id: 30, name: 'Luke', surname: 'Hernandez', email: '', password: '', phoneNumber: '', skills: ['Handyman'], rating: 4.6, category: ''
+  }];
+
 
   workers: IWorker[] = [];
 
   constructor() {
-    for (let i = 0; i < this.names.length; i++) {
-      const id = this.ids[i];
-      const name = this.names[i];
-      const rating = this.ratings[i];
-      const surname = this.surnames[i];
-      const email = this.emails[i];
-      const phoneNumber = this.phoneNumbers[i];
-      const skills = this.skills[i];
-      const category = this.categories[i];
-
-      this.workers.push({
-        id,
-        name,
-        surname,
-        email,
-        phoneNumber,
-        skills,
-        rating,
-        category
-      } as IWorker);
-    }
   }
 
   getWorker(workerId: number): Observable<IWorker> {
